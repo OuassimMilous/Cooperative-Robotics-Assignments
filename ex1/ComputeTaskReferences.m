7d391bf8eb7ff9e4529dc115aeae66c627f1978b
Ouassim Milous <59444113+OuassimMilous@users.noreply.github.com>
2025-01-29 20:58:35 +0100

    renaming some variables and reorganizing some files

--- ex1/ComputeTaskReferences.m
+++ ex1/ComputeTaskReferences.m
@@ -2,36 +2,36 @@
 function [uvms] = ComputeTaskReferences(uvms, mission)
 % compute the task references here
 
-% reference for tool-frame position control task
-[ang, lin] = CartError(uvms.vTg , uvms.vTt);
-uvms.xdot.t = 1 * [ang; lin];
-% limit the requested velocities...
-uvms.xdot.t(1:3) = Saturate(uvms.xdot.t(1:3), 1);
-uvms.xdot.t(4:6) = Saturate(uvms.xdot.t(4:6), 1);
-
-%REFRENCE FOR ROCK GOAL
-[ang_rock, lin_rock] = CartError(uvms.wTr , uvms.wTv);
-uvms.xdot.rock_ang = 1 * ang_rock;
-% display(uvms.xdot.rock_ang)
-uvms.xdot.rock_ang = Saturate(uvms.xdot.rock_ang, 1);
-
-%REFRENCE FOR VEHICLE POSITION CONTROL TASK
+% move
 [ang_v, lin_v] = CartError(uvms.wTgv , uvms.wTv);
 uvms.xdot.v_l = 1 * lin_v;
 uvms.xdot.v_a = 1 * ang_v;
 uvms.xdot.v_l = Saturate(uvms.xdot.v_l, 1);
 uvms.xdot.v_a = Saturate(uvms.xdot.v_a, 1);
 
-%TO MAKE IT HORIZONTAL
+% tool
+[ang, lin] = CartError(uvms.vTg , uvms.vTt);
+uvms.xdot.tool = 1 * [ang; lin];
+uvms.xdot.tool(1:3) = Saturate(uvms.xdot.tool(1:3), 1);
+uvms.xdot.tool(4:6) = Saturate(uvms.xdot.tool(4:6), 1);
+
+%rock
+[ang_rock, lin_rock] = CartError(uvms.wTr , uvms.wTv);
+uvms.xdot.rock = 1 * ang_rock;
+uvms.xdot.rock = Saturate(uvms.xdot.rock, 1);
+
+
+% horizental
 uvms.xdot.ha = 1 * (0 - norm(uvms.v_rho_ha)); % norm(v_rho) IS THETA
 
-%TO MAINTAIN THE MINIMUM ALTITUDE
+% minimum altitude
 uvms.xdot.ma = 1* (1 - uvms.a);
 
-%REFRENCE FOR THE ALTITUDE TASK
-uvms.xdot.a = 1* (0 - uvms.a); %I WANT TO LAND ON THE SEA FLOOR
+% landing
+uvms.xdot.landing = 1* (0 - uvms.a);
 
-%REFRENCE FOR UNDERACTUATION TASK
+% underactuation
 uvms.xdot.und = uvms.p_dot;
  
+% stop
 uvms.xdot.stop = zeros(6,1);
--- ex1/InitUVMS.m
+++ ex1/InitUVMS.m
@@ -23,7 +23,7 @@ uvms.jlmax  = [2.9;1.65;2.9;0.01;2.9;1.25;2.8];
 
 % to be computed at each time step
 uvms.wTv = eye(4,4);
-uvms.wTr = eye(4,4); %ROCK
+uvms.wTr = eye(4,4);
 uvms.wTt = eye(4,4);
 uvms.vTw = eye(4,4);
 uvms.vTe = eye(4,4);
@@ -36,13 +36,10 @@ uvms.djdq = zeros(6,7,7);
 uvms.mu  = 0;
 uvms.phi = zeros(3,1);
 uvms.sensorDistance = 0;
-uvms.rock_goal = []; %ROCK POSITION
-uvms.wRr = []; %ROCK ROTATION MATRIX
-% uvms.wTr = []; %ROCK TRANSFORMATION MATRIX
-uvms.xdot.rock_ang = []; %ROCK ANGULAR VELOCITY
-%ROCK WITH RESPECT TO VEHICLE
-uvms.vTr = [];
+uvms.rock_goal = [];
+uvms.wRr = []; 
 
+uvms.vTr = [];
 
 uvms.Jjl = [];
 uvms.Jmu = [];
@@ -50,83 +47,73 @@ uvms.Jha = [];
 uvms.Jt_a = [];
 uvms.Jt_v = [];
 uvms.Jt = [];
-%DEFINE THE JACOBIAN VARIABLES
-uvms.Jv_l = []; 
-uvms.Jv_a = [];
-%HORIZONTAL TASK JACOBIAN
-uvms.Jha = [];  
-%MINIMUM ALTITUDE TASK
-uvms.Jma = [];
-%ALTITUDE TASK
-uvms.Ja = [];
-%FOR UNDERACTUATION TASK
-uvms.Jund = [] ;
-%FOR ROCK TASK
-uvms.Jv_r = [];
 
 
 
-uvms.xdot.jl = [];
-uvms.xdot.mu = [];
-uvms.xdot.ha = [];
-uvms.xdot.t = [];
-% DEFINE THE VARIABLES
-uvms.xdot.v_l = [];
-uvms.xdot.v_a = [];
-% FOR MAINTAINING THE HORIZONTAL DISTANCE
-uvms.xdot.ha = [];
-%MINIMUM ALTITUDE TASK
-uvms.xdot.ma = [];
-%ALTITUDE TASK
-uvms.xdot.a = [];
-% FOR UNDERACTUATION
-uvms.xdot.und = [];
 
-
-
-    
+  
 uvms.A.jl = zeros(7,7);
+
 uvms.A.mu = 0;
-uvms.A.t = zeros(6,6);
-%DEFINE THE ACTIVATION FUNCTION
-uvms.A.v_l = zeros(3);
-uvms.A.v_a = zeros(3);
-%HORIZONATAL TASK ACTIVATION
-uvms.A.ha = 0;
-%MINIMUM ALTITUDE TASK
-uvms.A.ma= 0;
-%ALTITUDE TASK
-uvms.A.a= 0;
-%FOR UNDERACTUATION
-uvms.A.und = zeros(6);
-%ROCK TASK
-uvms.A.r = eye(3);
-
-
-
-
-%DEFINE THE ACTIVATION FUNCTION - FOR TASK SWITCHING
-uvms.Ap.v_l = zeros(3);
-uvms.Ap.v_a = zeros(3);
-%HORIZONATAL TASK ACTIVATION - FOR TASK SWITCHING
-uvms.Ap.ha = 0;
-%MINIMUM ALTITUDE TASK - FOR TASK SWITCHING
-uvms.Ap.ma= 0;
-%ALTITUDE TASK - FOR TASK SWITCHING
-uvms.Ap.a= 0;
-%ROCK TASK FOR TASK SWITICHING
-uvms.Ap.r = 0;
-
-
+uvms.xdot.mu = [];
 
 
 uvms.v_rho_ha = [];
 uvms.v_n_ha = [];
 uvms.a = [];
 
+uvms.xdot.jl = [];
+
+% move
+uvms.Ap.v_l = zeros(3);
+uvms.Ap.v_a = zeros(3);
+uvms.A.v_l = zeros(3);
+uvms.A.v_a = zeros(3);
+uvms.xdot.v_l = [];
+uvms.xdot.v_a = [];
+uvms.Jv_l = []; 
+uvms.Jv_a = [];
+
+% rock alignment
+uvms.xdot.rock = []; 
+uvms.Ap.rock = 0;
+uvms.A.rock = eye(3);
+uvms.Jrock = [];
+
+% tool
+uvms.A.tool = zeros(6,6);
+uvms.Ap.tool = zeros(6,6);
+uvms.xdot.tool = [];
+uvms.Jtool = zeros(6);
+
+% stop 
 uvms.A.stop = zeros(6);
+uvms.Ap.stop = zeros(6);
 uvms.xdot.stop=[];
 uvms.Jstop = zeros(6);
+
+% landing 
+uvms.Ap.landing= 0;
+uvms.A.landing= 0;
+uvms.xdot.landing = [];
+uvms.Jlanding = [];
+
+% minimum altitude
+uvms.Ap.ma= 0;
+uvms.A.ma= 0;
+uvms.xdot.ma = [];
+uvms.Jma = [];
+
+% horizental
+uvms.Ap.ha = 0;
+uvms.A.ha = 0;
+uvms.xdot.ha = [];
+uvms.Jha = [];  
+
+% underactuation
+uvms.A.und = zeros(6);
+uvms.xdot.und = [];
+uvms.Jund = [] ;
 
 end
 
