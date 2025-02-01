d0b2779e236d25d700c77e61400ffdfe65451c89
Ouassim Milous <59444113+OuassimMilous@users.noreply.github.com>
2025-02-01 19:57:55 +0100

    ex3 cleanup

--- ex3/ComputeH.m
+++ ex3/ComputeH.m
@@ -1,5 +1,5 @@
 function [H] = ComputeH(pandaArm)
-[U,D,V] = svd(pandaArm.wJt);
-J_pinv = V * pinv(D)*U';
-H = pandaArm.wJt * J_pinv;
+    [U,D,V] = svd(pandaArm.wJt);
+    J_pinv = V * pinv(D)*U';
+    H = pandaArm.wJt * J_pinv;
 end
--- ex3/UpdateMissionPhase.m
+++ ex3/UpdateMissionPhase.m
@@ -2,10 +2,8 @@ function [pandaArm, pandaArm2, mission] = UpdateMissionPhase(pandaArm, pandaArm2
         switch mission.phase
             case 1  %Go To Grasping Points
                 % computing the errors for the go-to action defining tasks
-                [ang_l, lin_l] = CartError(pandaArm.wTg,pandaArm.wTt);
-                 [ang_r, lin_r] = CartError(pandaArm2.wTg,pandaArm2.wTt);
-                 if(norm(ang_l)<0.0175 && norm(lin_l)<0.001 ...
-                         && norm(ang_r)<0.0175 && norm(lin_r)<0.001)
+                 if(norm(pandaArm.ang_l)<0.0175 && norm(pandaArm.lin_l)<0.001 ...
+                         && norm(pandaArm2.ang_r)<0.0175 && norm(pandaArm2.lin_r)<0.001)
                      mission.phase = 2;
                      mission.phase_time = 0;
                      disp('Changing to phase 2');
@@ -16,25 +14,21 @@ function [pandaArm, pandaArm2, mission] = UpdateMissionPhase(pandaArm, pandaArm2
                 % max error: 1/10 cm and 1deg
                 
             case 2 % Cooperative Manipulation Start 
-
                 % computing the errors for the rigid move-to task
-                 [ang_l, lin_l] = CartError(pandaArm.wTog,pandaArm.wTto);
-                 [ang_r, lin_r] = CartError(pandaArm2.wTog,pandaArm2.wTto);
-                 if(norm(ang_l)<0.0524 && norm(lin_l)<0.001 ...
-                         && norm(ang_r)<0.0524 && norm(lin_r)<0.001)
+
+                 if(norm(pandaArm.ang_l)<0.0524 && norm(pandaArm.lin_l)<0.001 ...
+                         && norm(pandaArm2.ang_r)<0.0524 && norm(pandaArm2.lin_r)<0.001)
                      mission.phase = 3;
                      mission.phase_time = 0;
-
                      disp('Changing to phase 3');
                      
                  end
-
                 % max error: 1 cm and 3deg
                
             case 3 % Finish motion
                 if(mission.phase_time >= 5)
                     mission.phase = 4;
-                     mission.phase_time = 0;
+                    mission.phase_time = 0;
                   disp('finishing');
                 end
             case 4 % end
